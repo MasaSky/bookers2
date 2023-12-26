d334bd11a2cd727f843e60206e5bee9d1b576d85
MasaSky <152603719+MasaSky@users.noreply.github.com>
2023-12-26 02:37:12 +0000

    Initial Setup

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  devise_for :users
+  root to: 'homes#top'
+  get 'homes/about'
+
+end
