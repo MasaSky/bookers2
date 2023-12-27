b303fb78b77315afcc2c9930010922fa9b0a0d5a
MasaSky <152603719+MasaSky@users.noreply.github.com>
2023-12-27 07:32:04 +0000

    231227-02

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,34 @@
 class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to user_path(current_user)
+    else
+      render :show
+    end
+  end
+
   def index
+    @books = Book.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
   end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2023_12_26_010504) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
+    t.string "introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
