00177410220d0e3c820659143f48017941c05ce8
Masashi YOSHIDA <152603719+MasaSky@users.noreply.github.com>
2023-12-30 01:51:23 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :email, presence: true, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
 
   def get_profile_image(width, height)
     unless profile_image.attached?
