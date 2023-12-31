92e419a63d33a4344452d6112f6aedf0a7959999
MasaSky <152603719+MasaSky@users.noreply.github.com>
2023-12-31 03:00:52 +0000

    231231-02

--- db/migrate/20231226005158_devise_create_users.rb
+++ db/migrate/20231226005158_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,              null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -33,7 +34,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.datetime :locked_at
 
       t.integer :user_id
-      t.string :name
       t.string :introduction
 
 
